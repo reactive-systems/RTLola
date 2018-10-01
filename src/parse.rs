a44ae72f4bbdc2fba01db881860e9299e8fb23a2
Leander Tentrup <tentrup@react.uni-saarland.de>
2018-10-01 12:49:18 +0200

    Update grammar
    Detailed changes:
    * update visibility of some rules in grammar
    * add test cases for parsing constant/input/output streams
    * removes PType from grammar

--- src/parse.rs
+++ src/parse.rs
@@ -32,4 +32,61 @@ mod tests {
         let pairs = LolaParser::parse(Rule::Spec, "input Int32 in\noutput Int32 out := in\n")
             .unwrap_or_else(|e| panic!("{}", e));
     }
+
+    #[test]
+    fn parse_constant() {
+        parses_to! {
+            parser: LolaParser,
+            input:  "constant Int five := 5",
+            rule:   Rule::ConstantStream,
+            tokens: [
+                ConstantStream(0, 22, [
+                    Type(9, 12, [
+                        Ident(9, 12, []),
+                    ]),
+                    Ident(13, 17, []),
+                    NumberLiteral(21, 22, []),
+                ]),
+            ]
+        };
+    }
+
+    #[test]
+    fn parse_input() {
+        parses_to! {
+            parser: LolaParser,
+            input:  "input Int in",
+            rule:   Rule::InputStream,
+            tokens: [
+                InputStream(0, 12, [
+                    Type(6, 9, [
+                        Ident(6, 9, []),
+                    ]),
+                    Ident(10, 12, [])
+                ]),
+            ]
+        };
+    }
+
+    #[test]
+    fn parse_output() {
+        parses_to! {
+            parser: LolaParser,
+            input:  "output Int out := in + 1",
+            rule:   Rule::OutputStream,
+            tokens: [
+                OutputStream(0, 24, [
+                    Type(7, 10, [
+                        Ident(7, 10, []),
+                    ]),
+                    Ident(11, 14, []),
+                    Expr(18, 24, [
+                        Ident(18, 20, []),
+                        Add(21, 22, []),
+                        NumberLiteral(23, 24, []),
+                    ]),
+                ]),
+            ]
+        };
+    }
 }
