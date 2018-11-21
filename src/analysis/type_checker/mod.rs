9b6a936b1a82661020c0168ba05ada0362a12341
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2018-11-21 08:07:02 +0100

    Rust format: Two spaces.

--- src/analysis/type_checker/mod.rs
+++ src/analysis/type_checker/mod.rs
@@ -352,7 +352,7 @@ mod tests {
             "There should be exactly one typing error."
         );
         match *res.errors[0] {
-            TypeError::InvalidArgument {..} => {}
+            TypeError::InvalidArgument { .. } => {}
             _ => assert!(false, "Incompatible types were not recognized as such."),
         }
     }
