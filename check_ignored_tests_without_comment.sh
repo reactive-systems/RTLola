1bc693c5b1e181c6c35ca88ce2a2646771bb4d58
Malte Schledjewski <schledjewski@react.uni-saarland.de>
2019-03-20 10:33:40 +0100

    move frontend into subfolder

--- check_ignored_tests_without_comment.sh
+++ check_ignored_tests_without_comment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-grep -n -r '#\[ignore\]' src | grep -v '\/\/'
+grep -n -r '#\[ignore\]' frontend/src | grep -v '\/\/'
 if [ "$?" -ne "1" ]; then
     echo "there are ignored test cases without accompanying comment"
     exit 1
