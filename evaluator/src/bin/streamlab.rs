9682d3af0f417eda974ff615b7b918f04e00bad6
Leander Tentrup <tentrup@react.uni-saarland.de>
2019-07-24 11:05:03 +0200

    evaluator: streamlab@react -> stream-lab@react

--- evaluator/src/bin/streamlab.rs
+++ evaluator/src/bin/streamlab.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         setup_panic!(Metadata {
             name: env!("CARGO_PKG_NAME").into(),
             version: env!("CARGO_PKG_VERSION").into(),
-            authors: "StreamLAB Team <streamlab@react.uni-saarland.de>".into(),
+            authors: "StreamLAB Team <stream-lab@react.uni-saarland.de>".into(),
             homepage: "www.stream-lab.org".into(),
         });
     }
